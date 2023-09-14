d2103b1fd2b380adba86352c9cbe7d7578da2eaa
unknown <rcomplexx@gmail.com>
2023-09-14 21:18:58 +0200

    :small screen adapting

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,13 @@ import '../styles/globals.css'
 
 export default function Home({ Component, pageProps }) {
   return ( 
-    <div className='componentWrapperDiv'>
+    <>
   <Navbar></Navbar>
   
   <div className='componentWrapperDiv2'>
   <Component {...pageProps} />
   </div>
   <Footer/>
-  </div>
+  </>
   )
 }
