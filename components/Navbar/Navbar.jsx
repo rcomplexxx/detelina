d73ce689209dfa6caca9940e713a37c77c5e460a
unknown <rcomplexx@gmail.com>
2023-09-15 00:52:53 +0200

    :small screen adapting

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,10 +1,60 @@
 import Image from "next/image";
 import Link from "next/link";
 import styles from "./navbar.module.css";
+import {useState, useRef, useEffect } from "react";
 
 const Navbar = () => {
+
+  const [windowWidth, setWindowWidth] = useState(0);
+
+
+  const navRef = useRef(null);
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+    setWindowWidth(window.innerWidth);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+ 
+
+const smallNavContent = '...';
+
+const bigNavContent =   <ul className={styles.menu}>
+<li>
+  <Link href="/about">
+ Pocetna
+  </Link>
+</li>
+<li>
+  <Link href="/services">
+    Proizvodi
+  </Link>
+</li>
+<li>
+  <Link href="/contact">
+   O nama
+  </Link>
+</li>
+<li>
+  <Link href="/contact">
+   Galerija
+  </Link>
+</li>
+<li>
+  <Link href="/contact">
+   Kontakt
+  </Link>
+</li>
+</ul>;
+
+
+
   return (
-    <nav className={styles.navbar}>
+    <nav  ref={navRef} className={styles.navbar}>
       <div className={styles.logo}>
         <Link href="/">
           <Image src='/images/logo.png'
@@ -13,33 +63,7 @@ const Navbar = () => {
           <h3 className={styles.title}>Detelina</h3>
         </Link>
       </div>
-      <ul className={styles.menu}>
-        <li>
-          <Link href="/about">
-         Pocetna
-          </Link>
-        </li>
-        <li>
-          <Link href="/services">
-            Proizvodi
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-           O nama
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-           Galerija
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-           Kontakt
-          </Link>
-        </li>
-      </ul>
+      {windowWidth<980?smallNavContent:bigNavContent}
     </nav>
   );
 };
