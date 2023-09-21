b05ff5ab463165395534d81a6d4df0885a7407d4
unknown <rcomplexx@gmail.com>
2023-09-22 01:45:44 +0200

    :design improvements

--- pages/o-nama.jsx
+++ pages/o-nama.jsx
@@ -5,7 +5,7 @@ import styles from '../styles/onama.module.css';
 export default function ONama ()  {
 
 return <div className={styles.mainDiv}>
-    <div className={styles.segementDiv}>
+    <div className={`${styles.segementDiv} ${styles.firstSegment}`}>
     <h1>O nama</h1>
 
     <p className={styles.oNamaText}>Neka hrana postane tvoj saveznik u ostvarivanju zdravog i kvalitetnog života! U srcu Leskovca, 
@@ -21,7 +21,7 @@ Vaš put ka zdravlju i kvalitetnom životu počinje ovde, uz podršku Zdravog Za
     fill/>
     </div></div>
 
-<div className={styles.segementDiv}> <p className={styles.oNamaText}>Neka hrana postane tvoj saveznik u ostvarivanju zdravog i kvalitetnog života! U srcu Leskovca, 
+<div className={`${styles.segementDiv} ${styles.thirdSegment}`}> <p className={styles.oNamaText}>Neka hrana postane tvoj saveznik u ostvarivanju zdravog i kvalitetnog života! U srcu Leskovca, 
 naša posvećena ekipa je stvorila oazu zdrave hrane kako bi podržala naše sugrađane u ostvarivanju njihovih najboljih verzija. U Zdravom 
 Zalogaju Leskovac, strastveno se trudimo da vam pružimo najfinije voće i povrće sa lokalnih njiva, kao i raznovrsne obroke koji hrane i 
 dušu i telo. Sa dubokim korenom u našoj zajednici, želimo vas inspirisati da donesete svesne izbore za svoje zdravlje svakodnevno. 
